7ba1eb0ba69b6ae9c72d98885591bbd3b2f01a83
Jack Magnus <jackm@minobar.com>
2017-02-19 00:07:06 +0100

    change wacomscript to account for one monitor

--- bin/wacomscript.sh
+++ bin/wacomscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "setting up graphics tablet!"
 xsetwacom set 'Wacom Intuos Pro L Finger touch' Touch off
-xsetwacom set 'Wacom Intuos Pro L Pen stylus' MapToOutput 1920x1080+1920+0
+xsetwacom set 'Wacom Intuos Pro L Pen stylus' MapToOutput 1920x1080+0+0
 
 
