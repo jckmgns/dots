204fa9564343f3735eac848b56482ba118827afc
Jack Magnus <jackm@minobar.com>
2017-02-20 23:10:50 +0100

    add install-script.sh

---
+++ install-script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# update package list
+sudo apt-get update
+
+# basic
+sudo apt-get -f -y install software-properties-common
+
+# i3
+sudo apt-get -f -y install i3 i3blocks rofi feh compton unclutter rxvt-unicode-256color
+
+# ncmpcpp
+sudo apt-get -f -y install mpd ncmpcpp mpc 
+sudo systemctl stop mpd.service
+sudo systemctl disable mpd.service
+
+# appearance
+sudo apt-get -f -y install arc-theme
+sudo apt-get -f -y install adwaita-icon-theme
+
+# applications
+sudo apt-get -f -y install vim chromium-browser thunderbird git lxappearance thunar ranger htop steam krita
+
+echo "don't forget to pull dotfiles from git-server!"
