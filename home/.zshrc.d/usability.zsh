351eb35c2c16acabca00ffb34498082d040a2f99
Jack Magnus <jack.magnus@jadomag.com>
2020-06-21 08:38:45 +0200

    Add custom ls color setup

--- home/.zshrc.d/usability.zsh
+++ home/.zshrc.d/usability.zsh
@@ -5,3 +5,12 @@ setopt autopushd # make cd work like pushd
 setopt extendedglob # extended globbing, eg. `cp ^*.(tar`
 setopt extendedhistory # add more info to history
 setopt nocorrectall # disable command correction
+
+# custom color setup for ls
+if [ -x "$(command -v dircolors)" ]; then
+    if [[ -s "$HOME/.dircolors" ]]; then
+        eval "$(dircolors --sh "$HOME/.dircolors")"
+    else
+        eval "$(dircolors --sh)"
+    fi
+fi
