4e48c5ff1e3c0794c09f1523e9553ba0894eb187
Jack Magnus <jack.magnus@jadomag.com>
2020-09-12 18:03:46 +0000

    Add explicit height to fzf dmenu

--- home/.i3/dmenu.bash
+++ home/.i3/dmenu.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo -n $PATH | sed "s/:/\n/g" | xargs -P 8 -I{} find {} -maxdepth 1 -executable | awk -F"/" "{print \$NF}" | sort | uniq -i | fzf --color=bw --reverse
+echo -n $PATH | sed "s/:/\n/g" | xargs -P 8 -I{} find {} -maxdepth 1 -executable | awk -F"/" "{print \$NF}" | sort | uniq -i | fzf --color=bw --reverse --height 100%
