f11ba24b35c3d1bce90de53db44f1e0a819961d6
Jack Magnus <jack.magnus@jadomag.com>
2021-01-16 12:58:25 +0100

    Replace custom dmenu with default

--- home/.i3/dmenu.bash
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-echo -n $PATH | sed "s/:/\n/g" | xargs -P 8 -I{} find {} -maxdepth 1 -executable | awk -F"/" "{print \$NF}" | sort | uniq -i | fzf --color=bw --reverse --height 100%
