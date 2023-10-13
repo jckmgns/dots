2b4ab85e08c81930448b39381193185d370b6781
Jack Magnus <git@jadomag.com>
2023-10-13 12:03:21 +0200

    Update arch setup script
    - Updates URL for st
    - Removes rust installation
    - Removes mpd setup (not installed anymore)

--- setup/arch.bash
+++ setup/arch.bash
@@ -3,40 +3,22 @@
 BLUE="\e[34m" # blue
 DFLT="\e[39m" # default
 
-# Install packages (that are not up-to-date)
+# install packages (which aren't up-to-date)
 sudo pacman -S --needed - < pkglist
 
-# Simple Terminal
+# st
 if ! hash st &> /dev/null; then
     echo -e "\n${BLUE}Install simple terminal (st)${DFLT}"
 
-    git clone --recurse-submodules https://github.com/jadomag/stw /tmp/stw
-    /tmp/stw/install
+    git clone --recurse-submodules https://github.com/jadomag/st.git /tmp/st
+    /tmp/st/install
 
-    rm -rf /tmp/stw
-fi
-
-# MPD
-echo -e "\n${BLUE}Setup MPD for local playback${DFLT}"
-
-sudo systemctl stop mpd.service
-sudo systemctl disable mpd.service
-
-# Rust
-if ! hash rustup &> /dev/null; then
-    echo -e "\n${BLUE}Start Rust installation${DFLT}"
-
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-    export PATH="${HOME}/.cargo/bin:${PATH}"
-
-    rustup component add clippy # clippy
-    rustup component add rls rust-analysis rust-src # language server
+    rm -rf /tmp/st
 fi
 
 # yay
 if ! hash yay &> /dev/null; then
-    echo -e "\n${BLUE}Start yay installation${DFLT}"
+    echo -e "\n${BLUE}Install yay${DFLT}"
 
     git clone https://aur.archlinux.org/yay.git /tmp/yay
     (cd /tmp/yay && makepkg -si)
