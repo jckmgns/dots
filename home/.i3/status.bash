9a4d7200d12db30c3a54f1727f10547f283affe8
Jack Magnus <git@jackmagnus.cc>
2024-03-27 13:55:48 +0100

    Change desktop look

--- home/.i3/status.bash
+++ home/.i3/status.bash
@@ -1,30 +1,43 @@
 #!/usr/bin/env bash
 
-status() {
-    status=',['
-
-    # date and time
-    status+='{'
-    status+='"name":"time",'
-    status+='"color":"#ebdbb2",'
-    status+='"full_text":'
-    status+="\" $(date '+%Y-%m-%d | %H:%M:%S') \""
-    status+='}'
-
-    status+=']'
-
-    echo ${status}
-}
-
-# ==============================
+color='#f5e0cc'
 
 echo '{ "version": 1 }'
 echo '['
 echo '[]'
 
-while :; do
-    status
-    sleep 1
+i3status | while :
+do
+    read line
+
+    status=',['
+
+    # title of active window
+
+    # id=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
+    # name=$(xprop -id $id | awk -F= '/_NET_WM_NAME/{$1="";print $2}' | sed -e 's/^\s*"//' -e 's/"\s*$//')
+
+    # status+='{'
+    # status+='"name":"title",'
+    # status+='"align":"left",'
+    # status+="\"color\":\"$color\","
+    # status+='"full_text":'
+    # status+="\"$name |\""
+    # status+='},'
+
+    # date and time
+    status+='{'
+    status+='"name":"time",'
+    status+="\"color\":\"$color\","
+    status+='"full_text":'
+    status+="\"$(date '+%Y-%m-%d | %H:%M') \""
+    status+='}'
+
+    status+=']'
+
+    echo ${status}
+
+    sleep 20
 done
 
 echo ']'
